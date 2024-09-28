5af2897426d5cc5d2d4dfc4150d052f839738a0b
Iowa Olivera <iowa.olivera@gmail.com>
2024-09-28 12:17:28 -0600

    Creación de modelos y migraciones para los objetos Contacto, Telefono, Direccion y Email

---
+++ app/Models/Contacto.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Contacto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contacto';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_contacto',
+        'numero'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    /**
+     * Get all of the telefonos for the Contactos
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function telefonos(): HasMany
+    {
+        return $this->hasMany(Telefono::class, 'id_contacto');
+    }
+
+    /**
+     * Get all of the direcciones for the Contacto
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function direcciones(): HasMany
+    {
+        return $this->hasMany(Direccion::class, 'id_contacto');
+    }
+
+    /**
+     * Get all of the emails for the Contacto
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function emails(): HasMany
+    {
+        return $this->hasMany(Email::class, 'id_contacto');
+    }
+}
---
+++ app/Models/Email.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model
+{
+    use HasFactory;
+
+    protected $table = 'email';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_contacto',
+        'numero'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function contacto()
+    {
+        return $this->belongsTo(Contacto::class, 'id_contacto');
+    }
+}
