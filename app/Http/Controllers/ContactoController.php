9c98af23260918188582c05d8c19867d7cdfdba1
Iowa Olivera <iowa.olivera@gmail.com>
2024-09-28 16:26:10 -0600

    Se hizo server side la tabla. Se movió el proceso a un servicio para contactos

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contacto;
+use App\Services\ContactoService;
 use App\Services\DireccionService;
 use App\Services\EmailService;
 use App\Services\TelefonoService;
@@ -14,27 +15,24 @@ class ContactoController extends Controller
     protected $telefonoService;
     protected $emailService;
     protected $direccionService;
+    protected $contactoService;
 
     public function __construct(
         TelefonoService $telefonoService,
         EmailService $emailService,
-        DireccionService $direccionService
+        DireccionService $direccionService,
+        ContactoService $contactoService
     ) {
         $this->telefonoService = $telefonoService;
         $this->emailService = $emailService;
         $this->direccionService = $direccionService;
+        $this->contactoService = $contactoService;
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $contactos = Contacto::with([
-            'telefonos',
-            'emails',
-            'direcciones'
-        ])->get();
-        return response([
-            'data' => $contactos
-        ]);
+        $contactos = $this->contactoService->getAllContacts($request);
+        return response($contactos);
     }
 
     public function store(Request $request)
@@ -55,19 +53,7 @@ class ContactoController extends Controller
             'direcciones.*.id' => 'nullable|integer|exists:direccion,id',
             'direcciones.*.direccion' => 'required|string|max:255'
         ]);
-
-        $contacto = Contacto::create([
-            'nombre' => $request->nombre,
-            'notas' => $request->notas,
-            'fecha_cumpleanios' => $request->fecha_cumpleanios,
-            'pagina_web' => $request->pagina_web,
-            'empresa' => $request->empresa,
-        ]);
-
-        $this->telefonoService->updateTelefonos($contacto, $request->telefonos);
-        $this->emailService->updateEmails($contacto, $request->emails);
-        $this->direccionService->updateDirecciones($contacto, $request->direcciones);
-
+        $contacto = $this->contactoService->createContact($request->all());
         return response([
             'data' => $contacto->load(['telefonos', 'emails', 'direcciones'])
         ]);
@@ -104,20 +90,12 @@ class ContactoController extends Controller
             'direcciones.*.direccion' => 'required|string|max:255'
         ]);
 
-        $contacto = Contacto::findOrFail($id);
-        $contacto->update($request->only([
-            'nombre',
-            'notas',
-            'fecha_cumpleanios',
-            'pagina_web',
-            'empresa'
-        ]));
+        $contacto = $this->contactoService->updateContact($request->all(), $id);
 
-        $this->telefonoService->updateTelefonos($contacto, $request->telefonos);
-        $this->emailService->updateEmails($contacto, $request->emails);
-        $this->direccionService->updateDirecciones($contacto, $request->direcciones);
-
-        return response()->json(['message' => 'Contacto actualizado con éxito', 'data' => $contacto], 200);
+        return response([
+            'message' => 'Contacto actualizado con éxito',
+            'data' => $contacto->load(['telefonos', 'emails', 'direcciones'])
+        ]);
     }
 
     public function destroy($id)
